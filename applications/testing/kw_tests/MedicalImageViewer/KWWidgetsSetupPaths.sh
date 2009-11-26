ad1c34e596cdd067ba31a0eadce21884e7dc67ba
Beck Olson <beckn8tor@users.sourceforge.net>
2009-11-26 01:41:56 +0000

    Removed kw bug example

--- applications/testing/kw_tests/MedicalImageViewer/KWWidgetsSetupPaths.sh
+++
@@ -1,8 +0,0 @@
-export PATH="/netopt/lib/vtk-5.2:/netopt/lib/vtk-5.2:/netopt/lib/KWWidgets:/netopt/lib/KWWidgets:/usr/bin:/usr/bin:$PATH"
-export LD_LIBRARY_PATH="/netopt/lib/vtk-5.2:/netopt/lib/KWWidgets:$LD_LIBRARY_PATH"
-export TCLLIBPATH=' {/netopt/lib/vtk-5.2} {/netopt/lib/KWWidgets/tcl} '$TCLLIBPATH
-export PYTHONPATH="/netopt/lib/vtk-5.2/../python2.4/site-packages:/netopt/lib/vtk-5.2:/netopt/lib/KWWidgets/../python2.4/site-packages:/netopt/lib/KWWidgets:$PYTHONPATH"
-if [ "0" == "1" ]; then
-  export ITCL_LIBRARY=""
-  export ITK_LIBRARY=""
-fi
