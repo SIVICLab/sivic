01852338cc5fe12590708ea0b3d84b9580810e61
Beck Olson <beckn8tor@users.sourceforge.net>
2010-03-02 18:20:00 +0000

    Added test

---
+++ libs/examples/test_vtkGL2PSExporterImages.cc
@@ -0,0 +1,247 @@
+/*
+ *  Copyright © 2009-2010 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name
+ *      "The Regents of the University of California," or the names of any of its
+ *      contributors may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ *  OF SUCH DAMAGE.
+ */
+
+
+/*
+ *  $URL: https://sivic.svn.sourceforge.net/svnroot/sivic/trunk/libs/examples/test_svkImageViewer2.cc $ 
+ *  $Rev: 116 $
+ *  $Author: beckn8tor $
+ *  $Date: 2010-02-08 14:21:06 -0800 (Mon, 08 Feb 2010) $
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson
+ *
+ */
+
+
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkImageClip.h>
+#include <vtkRenderWindowInteractor.h>
+#include <svkImageReader2.h>
+#include <svkLookupTable.h>
+#include <vtkGL2PSExporter.h>
+#include <svkImageReaderFactory.h>
+#include <svkImageData.h>
+#include <vtkCamera.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+#include <vtkExtractEdges.h>
+#include <vtkImageViewer2.h>
+#include <svkImageData.h>
+#include <vtkImageDataGeometryFilter.h>
+#include <vtkWindowLevelLookupTable.h>
+#include <vtkPlane.h>
+#include <vtkAxesActor.h>
+#include <vtkCubeSource.h>
+#include <vtkImageToPolyDataFilter.h>
+#include <vtkTransform.h>
+#include <svkImageViewer2.h>
+#include <vtkImageViewer2.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkInteractorStyleSwitch.h>
+
+using namespace svk;
+
+svkImageData* LoadFile( char* fileName );
+
+int main ( int argc, char** argv )
+{
+    char* fileNameImage = NULL;
+    if( argc == 2 ) {
+        fileNameImage = argv[1];
+    } else {
+        cerr<<"Too many input arguments."<<endl;
+        return 1;
+    } 
+    double* bounds;
+    double* range;
+    // initialize window
+    vtkRenderer* ren = vtkRenderer::New();
+    ren->SetBackground(0.1,0.2,0.4);
+    vtkRenderWindow* window = vtkRenderWindow::New();
+    vtkRenderWindowInteractor* rwi = vtkRenderWindowInteractor::New();
+    //svkImageViewer2* viewer = svkImageViewer2::New(); 
+    vtkImageViewer2* viewer = vtkImageViewer2::New(); 
+    //viewer->SetRenderWindow( window );
+    //viewer->SetRenderer( ren );
+    window->SetInteractor( rwi );
+    window->AddRenderer( ren);  
+    window->SetSize(600,600);  
+
+    // Load the data sets
+    svkImageData* image = LoadFile( fileNameImage );
+    
+    int* extent = image->GetExtent();
+    int newExtent[6];
+    memcpy( newExtent, extent, sizeof(double)*6);
+    newExtent[4] = 10;
+    newExtent[5] = 10;
+    vtkImageClip* clip = vtkImageClip::New();
+    clip->SetInput( image );
+    clip->SetOutputWholeExtent( newExtent );
+    vtkImageMapToColors* im2wlc = vtkImageMapToColors::New();
+    im2wlc->SetOutputFormatToRGB();
+    im2wlc->SetInput( clip->GetOutput() ); 
+    svkLookupTable* table = svkLookupTable::New();
+    table->SetLUTType( svkLookupTable::GREY_SCALE );
+    table->SetRange( 0, 256); 
+    im2wlc->SetLookupTable(table  ); 
+    cout << "filter output" << *im2wlc->GetOutput() << endl;
+    vtkImageToPolyDataFilter* filter = vtkImageToPolyDataFilter::New();
+    filter->SetLookupTable(table  ); 
+    filter->SetInput(im2wlc->GetOutput());
+    //filter->SetOutputStyleToPixelize();
+    filter->SetOutputStyleToRunLength();
+    filter->DecimationOn();
+    filter->Update();
+    vtkPolyDataMapper*        mapper = vtkPolyDataMapper::New();
+    mapper->SetInput( filter->GetOutput() );
+    vtkActor*                 pdActor = vtkActor::New();
+    pdActor->SetMapper( mapper );
+    //pdActor->GetProperty()->SetRepresentationToSurface();
+    //pdActor->GetProperty()->SetInterpolationToPhong();
+    pdActor->GetProperty()->SetDiffuse(0);
+    pdActor->GetProperty()->SetSpecular(0);
+    pdActor->GetProperty()->SetLineWidth(0);
+
+    // Throw in an axes
+    vtkAxesActor* myAxesNormal = vtkAxesActor::New();
+    vtkTransform* optimus = vtkTransform::New();
+    optimus->Translate(image->GetOrigin());
+    myAxesNormal->SetTotalLength(100,100,100); 
+    myAxesNormal->SetUserTransform(optimus);
+    optimus->Delete();
+    myAxesNormal->AxisLabelsOff();
+    ren->AddActor( myAxesNormal );
+    ren->AddActor( pdActor );
+    myAxesNormal->Delete();
+
+    // Setup cosine matrix, this should be grabbed from headers
+    double dcos[3][3];
+    dcos[0][0] = 1;
+    dcos[0][1] = 0;
+    dcos[0][2] = 0;
+    dcos[1][0] = 0;
+    dcos[1][1] = 0.99284;
+    dcos[1][2] = 0.11948;
+    dcos[2][0] = 0;
+    dcos[2][1] = 0.11948;
+    dcos[2][2] = -0.99284;
+
+    // Create oblique image
+    svkImageData* obliqueImage = svkMriImageData::New();
+    obliqueImage->CopyVtkImage( image, dcos );
+    int slice = 4; 
+    //viewer->SetSlice( 20 ); 
+    //viewer->SetInput( image ); 
+    //viewer->SetupInteractor( rwi );
+    //viewer->GetRenderer()->ResetCamera();
+    //viewer->SetSliceOrientationToXY();
+    //viewer->GetImageActor()->InterpolateOff();
+    //viewer->GetImageActor()->VisibilityOff();
+    //viewer->ResetCamera();
+    window->Render();
+    vtkInteractorStyleTrackballCamera* style = vtkInteractorStyleTrackballCamera::New();
+    //rwi->SetInteractorStyle( vtkInteractorStyleSwitch::New() );
+    
+    //rwi->SetInteractorStyle( style );
+
+    //viewer->SetOrientation( svkDcmHeader::CORONAL);
+    //viewer->SetSliceOrientationToXZ();
+    //viewer->ResetCamera();
+    vtkGL2PSExporter* epsWriter = vtkGL2PSExporter::New();
+    epsWriter->SetRenderWindow(window);
+    epsWriter->SetFilePrefix("testGraphics");
+    epsWriter->SetFileFormatToSVG();
+    //epsWriter->CompressOff();
+    epsWriter->OcclusionCullOff();
+    epsWriter->DrawBackgroundOff();
+    epsWriter->SimpleLineOffsetOff();
+    epsWriter->SetSortToBSP();
+    epsWriter->SetGlobalLineWidthFactor(10.0);
+    rwi->Start();
+    epsWriter->Write();
+
+    return 0;
+/*
+    for( int i = extent[2]; i < extent[3]; i++ ) {
+        viewer->SetSlice( i, svkDcmHeader::CORONAL );
+        window->Render();
+    }
+    //viewer->SetSliceOrientationToYZ();
+    viewer->SetOrientation( svkDcmHeader::SAGITTAL);
+    viewer->ResetCamera();
+    for( int i = extent[0]; i < extent[1]; i++ ) {
+        viewer->SetSlice( i, svkDcmHeader::SAGITTAL );
+        window->Render();
+    }
+    viewer->SetOrientation( svkDcmHeader::AXIAL);
+    viewer->ResetCamera();
+    for( int i = extent[4]; i < extent[5]; i++ ) {
+        viewer->SetSlice( i );
+        window->Render();
+    }
+    for( int i = extent[4]; i < extent[5]; i++ ) {
+        viewer->SetSlice( i, svkDcmHeader::AXIAL );
+        window->Render();
+    }
+    rwi->Start();
+
+    obliqueImage->Delete();
+    viewer->Delete();
+    ren->Delete();
+    window->Delete();
+    rwi->Delete();
+    image->Delete();
+    fileNameImage = NULL;
+    return 0;
+ */
+   
+}
+
+svkImageData* LoadFile( char* fileName )
+{
+    svkImageData* myData;
+    svkImageReaderFactory* readerFactory = svkImageReaderFactory::New();
+    svkImageReader2* reader = readerFactory->CreateImageReader2(fileName);
+    readerFactory->Delete();
+
+    if (reader == NULL) {
+        cerr << "Can not determine appropriate reader for: " << fileName << endl;
+        exit(1);
+    }
+
+    reader->SetFileName( fileName );
+    reader->Update();
+    myData = reader->GetOutput();
+    myData->Register(NULL);
+    reader->Delete();
+    return myData;
+}
