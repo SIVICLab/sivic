c2a8dcd9ec7ee06bf1be58c96d6b42c944bbf297
Beck Olson <beckn8tor@users.sourceforge.net>
2010-04-12 23:53:58 +0000

    Added Tests

---
+++ libs/examples/test_svkMrsImageFFTSpatial.cc
@@ -0,0 +1,172 @@
+/*
+ *  Copyright © 2009-2010 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name
+ *      "The Regents of the University of California," or the names of any of its
+ *      contributors may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ *  OF SUCH DAMAGE.
+ */
+
+
+/*
+ *  $URL: https://sivic.svn.sourceforge.net/svnroot/sivic/trunk/libs/examples/test_svkMrsImageFFT.cc $
+ *  $Rev: 77 $
+ *  $Author: jccrane $
+ *  $Date: 2010-01-26 11:16:55 -0800 (Tue, 26 Jan 2010) $
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson
+ *
+ */
+
+
+#include <svkDataModel.h>
+#include <svkImageData.h>
+#include <svkMrsImageFFT.h>
+#include <svkPlotGridView.h>
+#include <vtkRenderWindow.h>
+#include <vtkImageFourierCenter.h>
+#include <svkDdfVolumeWriter.h>
+#include <svkDICOMMRSWriter.h>
+
+using namespace svk;
+
+int main (int argc, char** argv)
+{
+
+    vtkObject::SetGlobalWarningDisplay(1);
+    string fname(argv[1]);
+
+    cout << "Testing svkMrsImageFFT on " << fname.c_str() << endl;
+    
+    svkDataModel* model = svkDataModel::New();
+
+
+    cout << "Loading file....." << endl;
+    svkImageData* data = model->LoadFile( fname.c_str() );
+
+    cout << "Checking to make sure its an MrsImageData....." << endl;
+    if( !data->IsA("svkMrsImageData") ) {
+        cerr << "INPUT MUST BE SPECTRA!" << endl;
+        exit(1);
+    }
+
+    // Lets look at the data...
+
+    svkPlotGridViewController* plotGridInput = svkPlotGridViewController::New(); 
+    vtkRenderWindow* window = vtkRenderWindow::New();
+    window->SetSize(800,800);
+    plotGridInput->SetRWInteractor( window->MakeRenderWindowInteractor() );
+    plotGridInput->SetInput( data, svkPlotGridView::MRS);
+    plotGridInput->SetSlice(3);
+    double range[2];
+    data->GetDataRange( range, 0 );
+    cout << "range: " << range[0] << " " << range[1] << endl;
+    plotGridInput->SetWindowLevelRange(range[0], range[1], svkPlotGridView::AMPLITUDE);
+    plotGridInput->GetView()->SetOrientation(svkDcmHeader::AXIAL);
+    plotGridInput->SetComponent(svkPlotLine::MAGNITUDE);
+    //plotGridInput->SetWindowLevelRange(0, 1023, svkPlotGridView::FREQUENCY);
+    plotGridInput->GetView()->Refresh();
+    window->Render();
+
+
+    //cout << "Printing output data: " << *data << endl;
+    window->GetInteractor()->Start();
+    for( int i = 0; i < 8; i++ ) {
+        plotGridInput->SetSlice(i);
+        window->GetInteractor()->Start();
+    }
+    
+    cout << "Instantiating the algorithm..." << endl;
+    svkMrsImageFFT* imageFFT = svkMrsImageFFT::New();
+   
+    cout << "Setting input to the algorithm..." << endl;
+    imageFFT->SetInput( data );
+    imageFFT->SetFFTDomain( svkMrsImageFFT::SPATIAL );
+    imageFFT->SetFFTMode( svkMrsImageFFT::REVERSE );
+    imageFFT->SetPostCorrectCenter( true );
+    imageFFT->SetPreCorrectCenter( true );
+    
+    cout << "Getting the output of the algorithm..." << endl;
+    svkImageData* outputData = imageFFT->GetOutput();
+
+    cout << "Updating the output of the algorithm..." << endl;
+    imageFFT->Update();
+    outputData->Modified();
+    outputData->Update(); 
+
+    //Lets write out the volume:
+    ///svkDdfVolumeWriter* writer = svkDdfVolumeWriter::New();
+
+
+    cout << "Now lets visualize the output." << endl;
+    outputData->GetDataRange( range, 0 );
+    cout << "range: " << range[0] << " " << range[1] << endl;
+    plotGridInput->SetWindowLevelRange(range[0], range[1], svkPlotGridView::AMPLITUDE);
+    //plotGridInput->SetWindowLevelRange(0, 1023, svkPlotGridView::FREQUENCY);
+    plotGridInput->GetView()->Refresh();
+    window->Render();
+    window->GetInteractor()->Start();
+    for( int i = 0; i < 8; i++ ) {
+        plotGridInput->SetSlice(i);
+        window->GetInteractor()->Start();
+    }
+
+    cout << "Instantiating the algorithm..." << endl;
+    svkMrsImageFFT* imageRFFT = svkMrsImageFFT::New();
+   
+    cout << "Setting input to the algorithm..." << endl;
+    imageRFFT->SetInput( data );
+    imageRFFT->SetFFTDomain( svkMrsImageFFT::SPECTRAL );
+    imageRFFT->SetFFTMode( svkMrsImageFFT::FORWARD );
+    
+    cout << "Getting the output of the algorithm..." << endl;
+    outputData = imageRFFT->GetOutput();
+
+    cout << "Updating the output of the algorithm..." << endl;
+    imageRFFT->Update();
+    outputData->Modified();
+    outputData->Update(); 
+
+    svkDICOMMRSWriter* writer = svkDICOMMRSWriter::New();
+    writer->SetFileName( "testerCenterStatic.dcm" );    
+    writer->SetInput( outputData );
+    writer->Write();
+
+
+    outputData->GetDataRange( range, 0 );
+    plotGridInput->SetWindowLevelRange(range[0], range[1], svkPlotGridView::AMPLITUDE);
+    //plotGridInput->SetWindowLevelRange(0, 1023, svkPlotGridView::FREQUENCY);
+    plotGridInput->GetView()->Refresh();
+    window->Render();
+    window->GetInteractor()->Start();
+    for( int i = 0; i < 8; i++ ) {
+        plotGridInput->SetSlice(i);
+        window->GetInteractor()->Start();
+    }
+    
+    
+     
+    return 0; 
+}
+
---
+++ libs/examples/test_vtkImageFourierCenter.cc
@@ -0,0 +1,272 @@
+/*
+ *  Copyright © 2009-2010 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name
+ *      "The Regents of the University of California," or the names of any of its
+ *      contributors may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ *  OF SUCH DAMAGE.
+ */
+
+
+/*
+ *  $URL: https://sivic.svn.sourceforge.net/svnroot/sivic/trunk/libs/examples/test_svkMrsImageFFT.cc $
+ *  $Rev: 77 $
+ *  $Author: jccrane $
+ *  $Date: 2010-01-26 11:16:55 -0800 (Tue, 26 Jan 2010) $
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson
+ *
+ */
+
+
+#include <svkDataModel.h>
+#include <svkImageData.h>
+#include <svkMrsImageFFT.h>
+#include <svkPlotGridView.h>
+#include <vtkRenderWindow.h>
+#include <vtkImageViewer2.h>
+#include <vtkBMPReader.h>
+#include <vtkPNMReader.h>
+#include <vtkImageExtractComponents.h>
+#include <vtkImageFourierCenter.h>
+#include <svkImageFourierCenter.h>
+#include <vtkImageData.h>
+#define PI 3.1415926535
+
+using namespace svk;
+
+void Initialize( );
+void PrintMatlabFormat( vtkImageData* image, string variable );
+void ViewImage( vtkImageData* image );
+void CenterImageVTK( vtkImageData* input, vtkImageData* output );
+void CenterImageSVK( vtkImageData* input, vtkImageData* output );
+void ReverseCenterImageSVK( vtkImageData* input, vtkImageData* output );
+void CreateImage( vtkImageData* input, int size );
+void CreateCross( vtkImageData* input, int size );
+void CreateBox( vtkImageData* input, int size );
+
+vtkImageViewer2* viewer;
+vtkRenderWindowInteractor* iren;
+
+int main (int argc, char** argv)
+{
+
+    Initialize();
+    // Lets Create a sine wave
+    vtkImageData* sineWaveImage = vtkImageData::New();
+    CreateBox( sineWaveImage, 13 );
+    PrintMatlabFormat( sineWaveImage, string("linearSignal") );
+    cout << *sineWaveImage << endl;
+    ViewImage( sineWaveImage );
+
+    vtkImageData* sineWaveImageCenter = vtkImageData::New();
+    ReverseCenterImageSVK( sineWaveImage, sineWaveImageCenter );
+    sineWaveImageCenter->Update();
+    PrintMatlabFormat( sineWaveImageCenter, string("linearSignalCenter") );
+    cout << *sineWaveImageCenter << endl;
+    ViewImage( sineWaveImageCenter );
+
+    vtkImageData* sineWaveImageRCenter = vtkImageData::New();
+    CenterImageSVK( sineWaveImageCenter, sineWaveImageRCenter );
+    sineWaveImageRCenter->Update();
+    PrintMatlabFormat( sineWaveImageRCenter, string("linearSignalRCenter") );
+    cout << *sineWaveImageRCenter << endl;
+    ViewImage( sineWaveImageRCenter );
+
+    vtkImageData* sineWaveImageRRCenter = vtkImageData::New();
+    ReverseCenterImageSVK( sineWaveImageRCenter, sineWaveImageRRCenter );
+    sineWaveImageRRCenter->Update();
+    PrintMatlabFormat( sineWaveImageRRCenter, string("linearSignalRRCenter") );
+    cout << *sineWaveImageRRCenter << endl;
+    ViewImage( sineWaveImageRRCenter );
+
+    vtkImageData* sineWaveImageRRRCenter = vtkImageData::New();
+    CenterImageSVK( sineWaveImageRRCenter, sineWaveImageRRRCenter );
+    sineWaveImageRRRCenter->Update();
+    PrintMatlabFormat( sineWaveImageRRRCenter, string("linearSignalRRRCenter") );
+    cout << *sineWaveImageRRRCenter << endl;
+    ViewImage( sineWaveImageRRRCenter );
+
+    return 0; 
+}
+
+void Initialize( )
+{
+    viewer = vtkImageViewer2::New();
+    viewer->GetImageActor()->InterpolateOff();
+    iren = vtkRenderWindowInteractor::New();
+    viewer->SetupInteractor( iren );
+}
+
+void PrintMatlabFormat( vtkImageData* image, string variable )
+{
+    double* tuple;
+    int linearIndex;
+    cout << variable.c_str() << "=[ " << endl;
+    for( int i = 0; i <= image->GetExtent()[1]; i++ ) {
+        for( int j = 0; j <= image->GetExtent()[3]; j++ ) {
+            linearIndex = j + i*(image->GetExtent()[1]+1);
+            tuple =  image->GetPointData()->GetScalars()->GetTuple(linearIndex);
+            cout << "(" << tuple[0] << ") + (" << tuple[1] << "i) ";
+        }
+        cout << ";" << endl;
+    }
+    cout << "]" << endl;
+
+}
+
+
+void ViewImage( vtkImageData* image )
+{
+    viewer->SetInput( image );
+    viewer->GetRenderer()->ResetCamera( );
+    viewer->GetRenderWindow()->SetSize( 800, 800 );
+    viewer->Render(); 
+    iren->Start();
+}
+
+void CenterImageVTK( vtkImageData* input, vtkImageData* output )
+{
+    vtkImageFourierCenter* ifc = vtkImageFourierCenter::New();
+    ifc->SetInput( input );
+    ifc->SetDimensionality(3);
+    ifc->Update();
+    output->DeepCopy( ifc->GetOutput() );
+    output->Update();
+    ifc->Delete();
+
+}
+
+void CenterImageSVK( vtkImageData* input, vtkImageData* output )
+{
+    svkImageFourierCenter* ifc = svkImageFourierCenter::New();
+    ifc->SetInput( input );
+    ifc->SetDimensionality(3);
+    ifc->Update();
+    output->DeepCopy( ifc->GetOutput() );
+    output->Update();
+    ifc->Delete();
+
+}
+
+void ReverseCenterImageSVK( vtkImageData* input, vtkImageData* output )
+{
+    svkImageFourierCenter* ifc = svkImageFourierCenter::New();
+    ifc->SetReverseCenter( true );
+    ifc->SetInput( input );
+    ifc->SetDimensionality(3);
+    ifc->Update();
+    output->DeepCopy( ifc->GetOutput() );
+    output->Update();
+    ifc->Delete();
+
+}
+
+void CreateImage( vtkImageData* input, int size )
+{
+    vtkDoubleArray* sinWave = vtkDoubleArray::New();
+    int rows = size;
+    int cols = size;
+    sinWave->SetNumberOfComponents(2);
+    sinWave->SetNumberOfTuples(rows*cols);
+    int linearIndex;
+    int constant = 1000000;
+    for( int i = 0; i < rows; i++ ) {
+        for( int j = 0; j < cols; j++ ) {
+            linearIndex = j+i*cols;
+            sinWave->SetComponent(j+i*cols,0, constant*linearIndex);
+            sinWave->SetComponent(j+i*cols,1, constant*linearIndex );
+        }
+    }
+    input->SetExtent(0, cols-1, 0, rows-1, 0, 0);
+    input->SetDimensions(cols, rows, 1);
+    input->SetNumberOfScalarComponents( 2 ); 
+    input->GetPointData()->SetScalars( sinWave ); 
+    input->SetScalarTypeToDouble( ); 
+    void* tmp = input->GetIncrements();
+    input->Modified();
+    input->Update();
+
+}
+
+void CreateCross( vtkImageData* input, int size )
+{
+    vtkDoubleArray* sinWave = vtkDoubleArray::New();
+    int rows = size;
+    int cols = size;
+    sinWave->SetNumberOfComponents(2);
+    sinWave->SetNumberOfTuples(rows*cols);
+    int linearIndex;
+    int constant = 1000000;
+    for( int i = 0; i < rows; i++ ) {
+        for( int j = 0; j < cols; j++ ) {
+            linearIndex = j+i*cols;
+            if( i == 0 || j==cols-1) { 
+                sinWave->SetComponent(j+i*cols,0, constant);
+                sinWave->SetComponent(j+i*cols,1, constant );
+            } else {
+                sinWave->SetComponent(j+i*cols,0, 0 );
+                sinWave->SetComponent(j+i*cols,1, 0 );
+            }
+        }
+    }
+    input->SetExtent(0, cols-1, 0, rows-1, 0, 0);
+    input->SetNumberOfScalarComponents( 2 ); 
+    input->GetPointData()->SetScalars( sinWave ); 
+    input->SetScalarTypeToDouble( ); 
+
+}
+
+void CreateBox( vtkImageData* input, int size )
+{
+    vtkDoubleArray* box = vtkDoubleArray::New();
+    int rows = size;
+    int cols = size;
+    box->SetNumberOfComponents(2);
+    box->SetNumberOfTuples(rows*cols);
+    int linearIndex;
+    int constant = 1000000;
+    for( int i = 0; i < rows; i++ ) {
+        for( int j = 0; j < cols; j++ ) {
+            linearIndex = j+i*cols;
+            if( i > rows/4-1 && i < 3*rows/4 && j > cols/4-1 && j < 3*cols/4 ) {
+                box->SetComponent(j+i*cols,0, constant);
+                box->SetComponent(j+i*cols,1, constant );
+            } else {
+                box->SetComponent(j+i*cols,0, 0 );
+                box->SetComponent(j+i*cols,1, 0 );
+            }
+        }
+    }
+    input->SetExtent(0, cols-1, 0, rows-1, 0, 0);
+    input->SetDimensions(cols, rows, 1);
+    input->SetNumberOfScalarComponents( 2 );
+    input->GetPointData()->SetScalars( box );
+    input->SetScalarTypeToDouble( );
+    void* tmp = input->GetIncrements();
+    input->Modified();
+    input->Update();
+
+}
+
