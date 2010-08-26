20e2d847a65d4af40f0f278e5084804c9f2dde85
Jason Crane <jccrane@users.sourceforge.net>
2010-08-26 04:59:30 +0000

    added test case

--- tests/src/CMakeSivicTests.cmake
+++ tests/src/CMakeSivicTests.cmake
@@ -111,6 +111,12 @@ SET( TEST_NAME TEST_10)
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/siemens_rda/sv)
 ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/sample.rda -o out -t 2" )
 
+########################
+#   DICOM MRS to DICOM MRS 
+########################
+SET( TEST_NAME TEST_11)
+SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/DICOM/mrs)
+ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/mrs.dcm.rda -o out.dcm -t 5" )
 
 
 ########################
