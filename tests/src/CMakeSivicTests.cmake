d57ca152ca556fc0a6f3969b342fbf77c4dd9106
Jason Crane <jccrane@users.sourceforge.net>
2010-09-02 17:57:38 +0000

    modified test cases to help support dynamic testing

--- tests/src/CMakeSivicTests.cmake
+++ tests/src/CMakeSivicTests.cmake
@@ -37,55 +37,94 @@
 #       Beck Olson
 
 
+#############################################################
+#   Paths to binary applications and scripts
+#############################################################
 SET( TEST_SCRIPT_PATH ${CMAKE_SOURCE_DIR}/trunk/tests/scripts)
 SET( TEST_BIN_PATH ${CMAKE_SOURCE_DIR}/trunk/applications/cmd_line/${PLATFORM})
+
+#############################################################
+#   Location where output files from individual tests 
+#   are written to. 
+#############################################################
 SET( TEST_RESULTS_ROOT ${SVK_TEST_ROOT}/results_tmp)
+
+#############################################################
+#   Flags for diff to avoid errors from minor differences in 
+#   paths and rootnames in header files. 
+#############################################################
 SET( DIFF_OPT --ignore-matching-lines=SVK_CMD --ignore-matching-lines=root)
 
 
+#############################################################
+#   Tests are in pairs:
+#     - odd tests are calls to svk binaries which can be tested with valgrind
+#       for memory checking (dynamic ctest analysis).  Files are generated from  
+#       these tests into $(TEST_RESULTS_ROOT).  Even tests check for run-time errors
+#       and memory allocation. 
+#     - even tests diff result files generated in the odd test against a baseline 
+#       file.  Even test check for processing correctness. 
+#############################################################
 
 ########################
 #   20X GE Raw to DDF
 ########################
 SET( TEST_NAME TEST_1)
-SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/20x)
 SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
+SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/20x)
 ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/20_x_raw -o${TEST_RESULTS_PATH}/out -t 2 )
 
 SET( TEST_NAME TEST_2)
 ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
 
+
 ########################
 #   15X GE Raw to DDF
 ########################
 SET( TEST_NAME TEST_3)
-SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/15x)
 SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
+SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/15x)
 ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/15x_raw -o${TEST_RESULTS_PATH}/out -t 2 )
 
 SET( TEST_NAME TEST_4)
 ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
 
+
 ########################
 #   14X GE Raw to DDF
 ########################
-SET( TEST_NAME TEST_3b)
+SET( TEST_NAME TEST_5)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/14x)
-ADD_TEST(${TEST_NAME}  ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/14x_raw -o out -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/14x_raw -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_6)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   12X GE Raw to DDF
 ########################
-SET( TEST_NAME TEST_4b)
+SET( TEST_NAME TEST_7)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/12x)
-ADD_TEST(${TEST_NAME}  ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/12x_raw -o out -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/12x_raw -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_8)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   11X GE Raw to DDF
 ########################
-SET( TEST_NAME TEST_5)
+SET( TEST_NAME TEST_9)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_pfiles/11x)
-ADD_TEST(${TEST_NAME}  ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/11x_raw -o out -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/11x_raw -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_10)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   9X GE Raw to DDF
@@ -98,108 +137,119 @@ ADD_TEST(${TEST_NAME}  ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PL
 ########################
 #   UCSF ddf to UCSF DDF
 ########################
-SET( TEST_NAME TEST_7)
+SET( TEST_NAME TEST_11)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ddf_files/ddf_to_ddf)
-ADD_TEST(${TEST_NAME}  ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/20x_1.ddf -o 20x -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/20x_1.ddf -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_12)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   DICOM MRI to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_8)
+SET( TEST_NAME TEST_13)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/DICOM/mri_phantom)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/E0000S4I1.DCM -o vol -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/E0000S4I1.DCM -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_14)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   UCSF IDF to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_9)
+SET( TEST_NAME TEST_15)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/idf_files/idf_to_idf)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/vol.idf -o out -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/vol.idf -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_16)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   VARIAN fdf to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_10)
+SET( TEST_NAME TEST_17)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/varian/fdf)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/lac.0001.fdf -o out -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/lac.0001.fdf -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_18)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   VARIAN fid to UCSF DDF 
 ########################
-SET( TEST_NAME TEST_11)
+SET( TEST_NAME TEST_19)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/varian/fid_cs)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/fid -o out -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/fid -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_20)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   GE Signa 5x to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_12)
+SET( TEST_NAME TEST_21)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_signa_files/Signa5x)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/E8668S3I1.MR -o out -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/E8668S3I1.MR -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_22)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   GE Signa 5x V2 to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_13)
+SET( TEST_NAME TEST_23)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_signa_files/Signa5XVersion2)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/E18636S3I1.MR -o out -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/E18636S3I1.MR -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_24)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   GE Signa 5x V2 to UCSF IDF 
 ########################
-SET( TEST_NAME TEST_14)
+SET( TEST_NAME TEST_25)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/ge_signa_files/SignaLX2)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/E12916S3I1.MR -o out -t 3" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/E12916S3I1.MR -o${TEST_RESULTS_PATH}/out -t 3 )
+
+SET( TEST_NAME TEST_26)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   SIEMENS RDA to DICOM DDF
 ########################
-SET( TEST_NAME TEST_15)
+SET( TEST_NAME TEST_27)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/siemens_rda/sv)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/sample.rda -o out -t 2" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/sample.rda -o${TEST_RESULTS_PATH}/out -t 2 )
+
+SET( TEST_NAME TEST_28)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
+
 
 ########################
 #   DICOM MRS to DICOM MRS 
 ########################
-SET( TEST_NAME TEST_16)
+SET( TEST_NAME TEST_29)
+SET( TEST_RESULTS_PATH ${TEST_RESULTS_ROOT}/${TEST_NAME})
 SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/DICOM/mrs)
-ADD_TEST(${TEST_NAME} ${TEST_SCRIPT_PATH}/test_svk_file_convert --platform ${PLATFORM} --bin_path ${TEST_BIN_PATH} --test_path ${SVK_TEST_ROOT} --test_name ${TEST_NAME} --baseline_path ${TEST_CASE_ROOT} --opts "-i ${TEST_CASE_ROOT}/input/mrs.dcm -o out.dcm -t 4" )
+ADD_TEST(${TEST_NAME}  ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/mrs.dcm -o${TEST_RESULTS_PATH}/out.dcm -t 4 )
 
-########################
-#   VALGRIND TEST BINARY DIRECTLY
-########################
-SET( TEST_NAME TEST_17)
-SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/DICOM/mrs)
-ADD_TEST(${TEST_NAME} ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/mrs.dcm -o /tmp/out.dcm -t 4 )
+SET( TEST_NAME TEST_30)
+ADD_TEST(${TEST_NAME}  diff ${DIFF_OPT} -r ${TEST_RESULTS_PATH} ${TEST_CASE_ROOT}/${PLATFORM} )
 
-########################
-#   VALGRIND TEST BINARY DIRECTLY
-#   otherwise valgrind tries to analyse perl script, etc.
-#   seems like it's necessary to run mem test as a separate
-#   test.
-########################
-SET( TEST_NAME TEST_18)
-SET( TEST_CASE_ROOT ${SVK_TEST_ROOT}/varian/fid_cs)
-ADD_TEST(${TEST_NAME} ${TEST_BIN_PATH}/svk_file_convert -i ${TEST_CASE_ROOT}/input/fid -o ${SVK_TEST_ROOT}/tmp/out -t 4 )
-
-
-
-#ADD_TEST(VTK_RENDER_LOOP ${EXECUTABLE_OUTPUT_PATH}/vtkRenderLoopTest)
-#ADD_TEST(RUNTIME_EXCEPTION ${EXECUTABLE_OUTPUT_PATH}/runtimeException)
-
-#ADD_TEST(SCRIPT_FAILURE ${EXECUTABLE_OUTPUT_PATH}/../tests/scripts/scriptFailTest)
-#ADD_TEST(ASSERT_FAILURE ${EXECUTABLE_OUTPUT_PATH}/assertFailTest)
-#ADD_TEST(RETURN_FAILURE ${EXECUTABLE_OUTPUT_PATH}/returnFailure)
-
-#ADD_TEST(READ_WRITE_SIVIC_IMAGE ${EXECUTABLE_OUTPUT_PATH}/../tests/scripts/idfReadWriteTest.pl
-#                                ${EXECUTABLE_OUTPUT_PATH}/svkIdfWriterTest ${SVK_TEST_ROOT}/sivic_out)
-#IF( TEST_OUTPUT_DIR )
-#    SET( TEST_OUTPUT_DIR -p${TEST_OUTPUT_DIR} )
-#ENDIF(TEST_OUTPUT_DIR)
-
-#ADD_TEST(PLOT_GRID_VIEW ${EXECUTABLE_OUTPUT_PATH}/../tests/scripts/plotGridViewRenderTest.pl
-#                                -t ${EXECUTABLE_OUTPUT_PATH}/svkPlotGridViewTest -s${SVK_TEST_ROOT}/t0000_1_cor.ddf 
-#                                ${TEST_OUTPUT_DIR} -r${TEST_REFERENCE_DIR})
-
-#ADD_TEST(READ_WRITE_NON_SIVIC_IMAGE ${EXECUTABLE_OUTPUT_PATH}/../tests/scripts/idfReadWriteTest.pl
-#                                    ${EXECUTABLE_OUTPUT_PATH}/svkIdfWriterTest ${SVK_TEST_ROOT}/t0000_fla)
