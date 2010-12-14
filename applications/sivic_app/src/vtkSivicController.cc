10e037c4201b03792b8382229cd1913cab5ec2cf
Jason Crane <jccrane@users.sourceforge.net>
2010-12-14 18:57:11 +0000

    added .IMA extension to load dialog for image data

--- applications/sivic_app/src/vtkSivicController.cc
+++ applications/sivic_app/src/vtkSivicController.cc
@@ -1071,11 +1071,11 @@ int vtkSivicController::OpenFile( char* openType, const char* startPath, bool re
     
         // Check to see which extention to filter for.
         if( strcmp( openType,"image" ) == 0 || strcmp( openType, "overlay" ) == 0 ) {
-            dlg->SetFileTypes("{{Volume Files} {.idf .fdf .dcm .DCM}} {{All files} {.*}}");
+            dlg->SetFileTypes("{{Image Files} {.idf .fdf .dcm .DCM .IMA}} {{All files} {.*}}");
         } else if( strcmp( openType,"spectra" ) == 0 || strcmp(openType, "spectra_one_channel") == 0 ) {
-            dlg->SetFileTypes("{{Complex Data} {.ddf .shf .rda .dcm}} {{All files} {.*}}");
+            dlg->SetFileTypes("{{MRS Files} {.ddf .shf .rda .dcm}} {{All files} {.*}}");
         } else {
-            dlg->SetFileTypes("{{All files} {.*}} {{Volume Files} {.idf .fdf .dcm .DCM}} {{Complex Data} {.ddf .shf .rda .dcm}}");
+            dlg->SetFileTypes("{{All files} {.*}} {{Image Files} {.idf .fdf .dcm .DCM .IMA}} {{MRS Files} {.ddf .shf .rda .dcm}}");
         }
     
     
