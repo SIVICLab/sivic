935e5b9a17c9edccf597858e36e6596566223ba2
Jason Crane <jccrane@users.sourceforge.net>
2011-04-12 23:50:26 +0000

    raw file dev

---
+++ libs/src/svkDICOMRawDataWriter.cc
@@ -0,0 +1,464 @@
+/*
+ *  Copyright © 2009-2011 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice, 
+ *      this list of conditions and the following disclaimer in the documentation 
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name 
+ *      "The Regents of the University of California," or the names of any of its 
+ *      contributors may be used to endorse or promote products derived from this 
+ *      software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ *  OF SUCH DAMAGE.
+ */
+
+
+/*
+ *  $URL$
+ *  $Rev$
+ *  $Author$
+ *  $Date$
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson
+ */
+
+
+
+#include <svkDICOMRawDataWriter.h>
+#include <svkRawIOD.h>
+#include <svkGEPFileReader.h>
+#include <svkImageReaderFactory.h>
+
+
+using namespace svk;
+
+
+vtkCxxRevisionMacro(svkDICOMRawDataWriter, "$Rev$");
+vtkStandardNewMacro(svkDICOMRawDataWriter);
+
+
+/*!
+ *
+ */
+svkDICOMRawDataWriter::svkDICOMRawDataWriter()
+{
+
+#if VTK_DEBUG_ON
+    this->DebugOn();
+#endif
+
+    vtkDebugMacro( <<  this->GetClassName() << "::" << this->GetClassName() << "()");
+
+    this->seriesNumber = 0; 
+    this->instanceNumber = 1; 
+    this->sha1Digest = ""; 
+}
+
+
+/*!
+ *
+ */
+svkDICOMRawDataWriter::~svkDICOMRawDataWriter()
+{
+}
+
+
+
+/*!
+ *  The main method which triggers the writer to create DICOM Raw Data file.
+ */
+void svkDICOMRawDataWriter::Write()
+{
+
+    this->SetErrorCode(vtkErrorCode::NoError);
+
+    svkImageReaderFactory* readerFactory =svkImageReaderFactory::New();
+    readerFactory->QuickParse();
+    svkGEPFileReader* reader = svkGEPFileReader::SafeDownCast( readerFactory->CreateImageReader2(this->FileName) );
+    if (reader == NULL) {
+        cerr << "Can not determine appropriate reader for: " << this->FileName << endl;
+        exit(1);
+    }
+    reader->OnlyParseHeader();
+    reader->SetFileName( this->FileName );
+    reader->Update();
+    this->pfMap = reader->GetPFMap();
+
+    this->InitDcmHeader();
+
+
+    if ( this->FileName ) { 
+        string fileNameString( this->FileName );
+        size_t pos = fileNameString.rfind(".dcm");
+        if( pos == string::npos ) {
+            pos = fileNameString.rfind(".DCM");
+            if( pos == string::npos ) {
+                fileNameString+=".dcm";
+                delete this->FileName;
+                this->FileName = new char[fileNameString.size() + 1];
+                strcpy(this->FileName, fileNameString.c_str());
+            }
+        }
+    } else if ( this->FilePattern ) {
+        string filePatternString( this->FilePattern );
+        size_t pos = filePatternString.rfind(".dcm");
+        if( pos == string::npos ) {
+            pos = filePatternString.rfind(".DCM");
+            if( pos == string::npos ) {
+                filePatternString+=".dcm";
+                delete this->FilePattern;
+                this->FilePattern = new char[filePatternString.size() + 1];
+                strcpy(this->FilePattern, filePatternString.c_str());
+            }
+        } 
+        if( this->FilePrefix ) {
+            string filePrefixString( this->FilePrefix );
+            size_t pos = filePrefixString.rfind(".dcm");
+            if( pos != string::npos ) {
+                filePrefixString.replace(pos, pos + 4, "");
+                delete this->FilePrefix;
+                this->FilePrefix = new char[filePrefixString.size() + 1];
+                strcpy(this->FilePrefix, filePrefixString.c_str());
+            }
+            pos = filePrefixString.rfind(".DCM");
+            if( pos != string::npos ) {
+                filePrefixString.replace(pos, pos + 4, "");
+                delete this->FilePrefix;
+                this->FilePrefix = new char[filePrefixString.size() + 1];
+                strcpy(this->FilePrefix, filePrefixString.c_str());
+            }
+        }
+    } else {
+        vtkErrorMacro(<<"Write:Please specify either a FileName or a file prefix and pattern");
+        this->SetErrorCode(vtkErrorCode::NoFileNameError);
+        return;
+    }
+   
+     
+    // Make sure the file name is allocated
+    this->InternalFileName =
+        new char[(this->FileName ? strlen(this->FileName) : 1) +
+            (this->FilePrefix ? strlen(this->FilePrefix) : 1) +
+            (this->FilePattern ? strlen(this->FilePattern) : 1) + 10];
+    
+
+    this->FileNumber = 1; 
+    this->MinimumFileNumber = this->MaximumFileNumber = this->FileNumber;
+    this->FilesDeleted = 0;
+    this->UpdateProgress(0.0);
+
+ 
+    this->dcmHeader->InsertUniqueUID( "SOPInstanceUID" );
+
+    this->dcmHeader->SetValue(
+        "InstanceNumber",
+        this->FileNumber 
+    );
+
+    // determine the name
+    if (this->FileName) {
+        sprintf(this->InternalFileName,"%s",this->FileName);
+    } else {
+        if (this->FilePrefix) {
+            sprintf(this->InternalFileName, this->FilePattern, this->FilePrefix, this->FileNumber );
+        } else {
+            sprintf(this->InternalFileName, this->FilePattern,this->FileNumber);
+        }
+    }
+
+    this->dcmHeader->WriteDcmFile(this->InternalFileName);
+
+    if (this->ErrorCode == vtkErrorCode::OutOfDiskSpaceError) {
+        vtkErrorMacro("Ran out of disk space; deleting file(s) already written");
+        this->DeleteFiles();
+        return;
+    }
+
+    delete [] this->InternalFileName;
+    this->InternalFileName = NULL;
+
+}
+
+
+/*!
+ *  Generates a new svkDcmHeader initialized to a default DICOM Secondary
+ *  Capture IOD, then fills in instance specific values.       
+ *  The StudyInstanceUID and other patient/exam specific attributes are copied from the 
+ *  template to attach the images to the data from which it was derived.    
+ */
+void svkDICOMRawDataWriter::InitDcmHeader()
+{
+
+    if ( svkDcmHeader::adapter_type == svkDcmtkAdapter::DCMTK_API ) {
+        this->dcmHeader =  svkDcmtkAdapter::New() ;
+    }
+
+    /*  
+     *  Get Default populated SOP Instance:
+     */
+    svkIOD* iod = svkRawIOD::New();
+    iod->SetDcmHeader( this->dcmHeader );
+    iod->InitDcmHeader();
+    iod->Delete();
+
+
+    this->InitPatientModule(); 
+    this->InitGeneralStudyModule(); 
+    this->InitGeneralSeriesModule(); 
+    this->InitGeneralEquipmentModule(); 
+    this->InitRawDataModule(); 
+
+    if (this->GetDebug()) {
+        //this->dcmHeader->PrintDcmHeader();
+    }
+
+}
+
+
+/*!
+ *
+ */
+void svkDICOMRawDataWriter::InitPatientModule()
+{
+
+    int patsex = this->GetHeaderValueAsInt("rhe.patsex");
+
+    vtkstd::string gender("O");
+    if ( patsex == 1 ) {
+        gender.assign("M");
+    } else if ( patsex == 2 ) {
+        gender.assign("F");
+    }
+
+    this->dcmHeader->InitPatientModule(
+        this->pfMap["rhe.patname"][3],  
+        this->pfMap["rhe.patid"][3],  
+        this->pfMap["rhe.dateofbirth"][3],  
+        gender
+    );
+
+}
+
+
+/*!
+ *
+ */
+void svkDICOMRawDataWriter::InitGeneralStudyModule()
+{
+
+    vtkstd::string dcmDate = svkGEPFileMapper::ConvertGEDateToDICOM( this->pfMap["rhr.rh_scan_date"][3] ); 
+
+    this->dcmHeader->InitGeneralStudyModule(
+        svkImageReader2::RemoveSlashesFromDate( &dcmDate ),
+        this->pfMap["rhr.rh_scan_time"][3],  
+        this->pfMap["rhe.refphy"][3],  
+        this->pfMap["rhe.ex_no"][3],  
+        this->pfMap["rhe.reqnum"][3],  
+        this->pfMap["rhe.study_uid"][3]  
+    );
+
+}
+
+
+/*!
+ *
+ */
+void svkDICOMRawDataWriter::InitGeneralSeriesModule()
+{
+
+    vtkstd::string patientEntryPos;
+    int patientEntry( this->GetHeaderValueAsInt( "rhs.entry" ) );
+    if ( patientEntry == 0) {
+        patientEntryPos = "Unknown";
+    } else if ( patientEntry == 1) {
+        patientEntryPos = "HF";
+    } else if ( patientEntry == 2) {
+        patientEntryPos = "FF";
+    }
+
+    int patientPosition( this->GetHeaderValueAsInt( "rhs.position" ) );
+    if ( patientPosition == 0 ) {
+        patientEntryPos.append("Unknown");
+    } else if ( patientPosition == 1 ) {
+        patientEntryPos.append("S");
+    } else if ( patientPosition == 2 ) {
+        patientEntryPos.append("P");
+    }
+
+    this->dcmHeader->InitGeneralSeriesModule(
+        this->pfMap["rhs.se_no"][3],  
+        this->pfMap["rhs.se_desc"][3],  
+        patientEntryPos
+    );
+
+}
+
+
+/*!
+ *  initialize
+ */
+void svkDICOMRawDataWriter::InitGeneralEquipmentModule()
+{
+    this->dcmHeader->SetValue(
+        "Manufacturer",
+        "GE MEDICAL SYSTEMS"
+    );
+
+    this->dcmHeader->SetValue(
+        "InstitutionName",
+        this->pfMap["rhe.hospname"][3]  
+    );
+
+    this->dcmHeader->SetValue(
+        "StationName",
+        this->pfMap["rhe.ex_sysid"][3]  
+    );
+
+}
+
+
+/*!
+ *  returns the value for the specified key as an int.
+ */
+void svkDICOMRawDataWriter::InitRawDataModule()
+{
+    this->dcmHeader->SetValue(
+        "InstanceNumber",
+        1 
+    );
+
+    vtkstd::string dcmDate = svkGEPFileMapper::ConvertGEDateToDICOM( this->pfMap["rhr.rh_scan_date"][3] ); 
+
+    this->dcmHeader->SetValue(
+        "ContentDate",
+        svkImageReader2::RemoveSlashesFromDate( &dcmDate )
+    ); 
+
+
+    time_t time = this->GetHeaderValueAsInt("rhe.ex_datetime");
+
+    //convert to Pacific time:  subtract 8 hours
+    struct tm * timeinfo;
+    timeinfo = localtime ( &time );
+    bool isDaylightSavingsTime = timeinfo->tm_isdst;
+    if ( isDaylightSavingsTime ) { 
+        time += 7 * 60 * 60;
+    } else {
+        time += 8 * 60 * 60;
+    }
+    char timeBuf[80];
+    strftime (timeBuf, 80,"%H%M", timeinfo);
+
+    this->dcmHeader->SetValue(
+        "ContentTime",
+        string(timeBuf)
+    ); 
+
+
+    ifstream* pFile = new ifstream();
+    pFile->exceptions( ifstream::eofbit | ifstream::failbit | ifstream::badbit );
+
+    try {
+
+        pFile->open( this->FileName, ios::binary);
+
+        pFile->seekg(0, ios::end);
+        // get-ptr position is now same as file size
+        ios::pos_type pfileSize = pFile->tellg();  
+        cout << "PFILE SIZE = " << static_cast<int>(pfileSize) << endl;
+
+        void* pfileBuffer = new char[static_cast<int>(pfileSize)]; 
+        pFile->seekg(0, ios::beg);
+        pFile->read( static_cast<char*>(pfileBuffer), pfileSize);
+
+        this->dcmHeader->SetValue(
+            "SVK_PRIVATE_TAG",
+            "SVK_PRIVATE_CREATOR"
+        );
+
+        this->dcmHeader->InsertEmptyElement( "SVK_FILE_SET_SEQUENCE" );
+
+
+        this->dcmHeader->AddSequenceItemElement(
+            "SVK_FILE_SET_SEQUENCE",
+            0,
+            "SVK_FILE_SHA1_DIGEST",
+            this->sha1Digest
+        );
+
+        this->dcmHeader->AddSequenceItemElement(
+            "SVK_FILE_SET_SEQUENCE",
+            0,
+            "SVK_FILE_NUM_BYTES",
+            static_cast<int>(pfileSize) 
+        );
+
+        this->dcmHeader->AddSequenceItemElement(
+            "SVK_FILE_SET_SEQUENCE",
+            0,
+            "SVK_FILE_CONTENTS",
+            static_cast<float*>(pfileBuffer),
+            pfileSize/sizeof(float) 
+        );
+
+    } catch (ifstream::failure e) {
+        cout << "ERROR: Exception opening/reading file " << this->FileName << " => " << e.what() << endl;
+        exit(1);
+    }
+
+    pFile->close();
+}
+
+
+/*!
+ *
+ */
+void svkDICOMRawDataWriter::SetSHA1Digest( string sha1Digest )
+{
+    this->sha1Digest = sha1Digest; 
+}
+
+
+/*!
+ *  returns the value for the specified key as an int.
+ */
+int svkDICOMRawDataWriter::GetHeaderValueAsInt(vtkstd::string key)
+{
+
+    istringstream* iss = new istringstream();
+    int value;
+
+    iss->str( this->pfMap[key][3] );
+    *iss >> value;
+    delete iss;
+    return value;
+}
+
+
+
+/*!
+ *
+ */
+int svkDICOMRawDataWriter::FillInputPortInformation( int vtkNotUsed(port), vtkInformation* info )
+{
+    info->Set(svkDICOMRawDataWriter::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+    return 1;
+}
+
+
---
+++ libs/src/svkDcmRawDataReader.cc
@@ -0,0 +1,204 @@
+/*
+ *  Copyright © 2009-2011 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice, 
+ *      this list of conditions and the following disclaimer in the documentation 
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name 
+ *      "The Regents of the University of California," or the names of any of its 
+ *      contributors may be used to endorse or promote products derived from this 
+ *      software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ *  OF SUCH DAMAGE.
+ */
+
+
+
+/*
+ *  $URL$
+ *  $Rev$
+ *  $Author$
+ *  $Date$
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson
+ */
+
+
+#include <svkDcmRawDataReader.h>
+#include <svkMriImageData.h>
+#include <vtkType.h>
+#include <vtkDebugLeaks.h>
+#include <vtkStringArray.h>
+#include <vtkInformation.h>
+#include <svkIOD.h>
+#include <svkRawIOD.h>
+#include <svkEnhancedMRIIOD.h>
+#include <vtkstd/vector>
+#include <vtkstd/utility>
+#include <vtkstd/algorithm>
+#include <sstream>
+
+
+using namespace svk;
+
+
+vtkCxxRevisionMacro(svkDcmRawDataReader, "$Rev$");
+vtkStandardNewMacro(svkDcmRawDataReader);
+
+
+/*!
+ *
+ */
+svkDcmRawDataReader::svkDcmRawDataReader()
+{
+#if VTK_DEBUG_ON
+    this->DebugOn();
+    vtkDebugLeaks::ConstructClass("svkDcmRawDataReader");
+#endif
+    vtkDebugMacro(<<this->GetClassName() << "::" << this->GetClassName() << "()");
+}
+
+
+/*!
+ *
+ */
+svkDcmRawDataReader::~svkDcmRawDataReader()
+{
+    vtkDebugMacro(<<this->GetClassName() << "::~" << this->GetClassName() << "()");
+}
+
+
+/*!
+ *  Mandator, must be overridden to get the factory to check for proper
+ *  type of vtkImageReader2 to return. 
+ */
+int svkDcmRawDataReader::CanReadFile(const char* fname)
+{
+
+    vtkstd::string fileToCheck(fname);
+
+    bool isDcmRaw = false; 
+
+    if ( svkDcmHeader::IsFileDICOM( fname ) ) {
+
+        svkImageData* tmp = svkMriImageData::New(); 
+
+        tmp->GetDcmHeader()->ReadDcmFile( fname );
+        vtkstd::string SOPClassUID = tmp->GetDcmHeader()->GetStringValue( "SOPClassUID" ) ;
+
+        //verify that this isn't a proprietary use of DICOM MR ImageStorage: 
+        if( tmp->GetDcmHeader()->ElementExists( "SVK_PRIVATE_TAG", "top") ) {
+                    
+            if ( SOPClassUID == "1.2.840.10008.5.1.4.1.1.66" ) {           
+                this->SetFileName(fname);
+                isDcmRaw = true; 
+            }
+        
+        }
+
+        tmp->Delete(); 
+    }
+
+    if ( isDcmRaw ) {
+        cout << this->GetClassName() << "::CanReadFile(): It's a DICOM Raw Data File: " <<  fileToCheck << endl;
+        return 1;
+    }
+
+    vtkDebugMacro(<<this->GetClassName() << "::CanReadFile() It's Not a DICOM Raw Data file " << fileToCheck );
+
+    return 0;
+}
+
+
+/*!
+ *
+ */
+void svkDcmRawDataReader::LoadData( svkImageData* data )
+{
+}
+
+/*!
+ *  Side effect of Update() method.  Used to load pixel data and initialize vtkImageData
+ *  Called before ExecuteData()
+ */
+void svkDcmRawDataReader::ExecuteInformation()
+{
+    this->InitDcmHeader();
+}
+
+
+
+/*! 
+ *  Init the Shared and PerFrame sequences and load the pixel data into the svkImageData object. 
+ */
+void svkDcmRawDataReader::ExtractFiles()
+{
+
+    string outputFileName( this->FileName );  
+    outputFileName.append(".extracted"); 
+    ofstream outputFile ( outputFileName.c_str(), ios::binary );
+    if( !outputFile) { 
+        throw runtime_error("Cannot open file to extract raw data to ");
+    }
+    
+
+    long unsigned int numBytes=  
+        this->GetOutput()->GetDcmHeader()->GetIntSequenceItemElement(
+            "SVK_FILE_SET_SEQUENCE",
+            0,
+            "SVK_FILE_NUM_BYTES"
+        );
+    long unsigned int numValues =  numBytes / sizeof(float); 
+    float* fileBuffer = new float[ numValues ]; 
+
+    this->GetOutput()->GetDcmHeader()->GetFloatSequenceItemElement(
+            "SVK_FILE_SET_SEQUENCE",
+            0,
+            "SVK_FILE_CONTENTS",
+            fileBuffer, 
+            numValues
+        );
+
+    
+    outputFile.write( (char *)fileBuffer, numBytes); 
+
+    if (fileBuffer != NULL) {
+        delete[] fileBuffer; 
+    }
+
+}
+
+/*!
+ *  Returns the pixel type
+ */
+svkDcmHeader::DcmPixelDataFormat svkDcmRawDataReader::GetFileType()
+{
+    return svkDcmHeader::UNDEFINED;
+}
+
+
+/*!
+ *
+ */
+int svkDcmRawDataReader::FillOutputPortInformation( int vtkNotUsed(port), vtkInformation* info )
+{
+    info->Set(vtkDataObject::DATA_TYPE_NAME(), "svkMriImageData");
+    return 1;
+}
+
