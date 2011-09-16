c5107eba7c3f749a47cdc8954497af34d61e64b8
Jason Crane <jccrane@users.sourceforge.net>
2011-09-16 23:03:27 +0000

    if not proton spec, thn set centered on water to no

--- libs/src/svkDdfVolumeWriter.cc
+++ libs/src/svkDdfVolumeWriter.cc
@@ -607,6 +607,10 @@ void svkDdfVolumeWriter::InitHeader(ofstream* out, vtkstd::string fileName)
     *out << "dwelltime(ms): " << fixed << setprecision(6) << 1000/( hdr->GetFloatValue( "SpectralWidth" ) ) << endl;
     *out << "frequency offset(Hz): " << fixed << setprecision(6) << hdr->GetFloatValue( "SVK_FrequencyOffset" ) << endl;
     vtkstd::string onH20 = "yes"; 
+    //  if this is not H1, then set to no:
+    if ( hdr->GetStringValue( "ResonantNucleus" ).compare("H1") != 0 ) {
+        onH20.assign( "no" ); 
+    }
     if (hdr->GetFloatValue( "SVK_FrequencyOffset" ) != 0 ) {
         onH20.assign( "no" ); 
     }
