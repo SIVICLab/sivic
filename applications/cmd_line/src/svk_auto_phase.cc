ecadd7a0ed30262af9a8871badd0c1187f446730
Jason Crane <jccrane@users.sourceforge.net>
2013-08-14 21:50:37 +0000

    updated copyright and added auto_phase

--- applications/cmd_line/src/svk_auto_phase.cc
+++ applications/cmd_line/src/svk_auto_phase.cc
@@ -28,10 +28,10 @@
 
 
 /*
- *  $URL$
- *  $Rev$
- *  $Author$
- *  $Date$
+ *  $URL: https://sivic.svn.sourceforge.net/svnroot/sivic/trunk/applications/cmd_line/src/svk_met_kinetics.cc $
+ *  $Rev: 1350 $
+ *  $Author: jccrane $
+ *  $Date: 2012-09-13 15:16:12 -0700 (Thu, 13 Sep 2012) $
  *
  *  Authors:
  *   
@@ -48,9 +48,7 @@
 #include <svkImageReader2.h>
 #include <svkImageWriterFactory.h>
 #include <svkImageWriter.h>
-
-//  Insert your algorithm here in place of "AlgoTemplate":
-#include <svkDynamicMRIAlgoTemplate.h>
+#include <svkMRSAutoPhase.h>
 
 #ifdef WIN32
 extern "C" {
@@ -69,42 +67,31 @@ int main (int argc, char** argv)
 
     string usemsg("\n") ;
     usemsg += "Version " + string(SVK_RELEASE_VERSION) + "\n";
-    usemsg += "svk_met_kinetics -i1 met1 -i2 met2 -i3 met3 -o output_file_name [ -t output_data_type ] [ -h ] \n";
-    usemsg += "\n";
-    usemsg += "   -i1               name   Name of met 1. \n";
-    usemsg += "   -i2               name   Name of met 2. \n";
-    usemsg += "   -i3               name   Name of met 3. \n";
-    usemsg += "   -o                name   Name of outputfile. \n";
-    usemsg += "   -t                type   Target data type: \n";
-    usemsg += "                                 3 = UCSF IDF    \n";
-    usemsg += "                                 4 = DICOM_MRI (default)    \n";
-    usemsg += "   -h                       Print this help mesage. \n";
-    usemsg += "\n";
-    usemsg += "Fit dynamic MRSI to metabolism kinetics model.\n";
-    usemsg += "\n";
+    usemsg += "svk_auto_phase -i input_filename -o output_filename [ -t output_data_type ] [ -h ] \n";
+    usemsg += "                                                         \n";
+    usemsg += "   -i            name   Name of input MRS file.          \n";
+    usemsg += "   -o            name   Name of output file.             \n";
+    usemsg += "   -t                type   Target data type:            \n";
+    usemsg += "                                 2 = UCSF DDF            \n";
+    usemsg += "                                 4 = DICOM_MRS (default) \n";
+    usemsg += "   -h                       Print this help mesage.      \n";
+    usemsg += "                                                         \n";
+    usemsg += "Auto phase spectra (zero and first order phaseing).      \n";
+    usemsg += "                                                         \n";
 
 
-    string inputFileName1;
-    string inputFileName2;
-    string inputFileName3;
+    string inputFileName;
     string outputFileName;
-    svkImageWriterFactory::WriterType dataTypeOut = svkImageWriterFactory::DICOM_MRI;
+    svkImageWriterFactory::WriterType dataTypeOut = svkImageWriterFactory::DICOM_MRS;
 
     string cmdLine = svkProvenance::GetCommandLineString( argc, argv );
 
     enum FLAG_NAME {
-        FLAG_IM_1 = 0, 
-        FLAG_IM_2, 
-        FLAG_IM_3
     };
 
 
     static struct option long_options[] =
     {
-        /* This option sets a flag. */
-        {"i1",      required_argument, NULL,  FLAG_IM_1},
-        {"i2",      required_argument, NULL,  FLAG_IM_2},
-        {"i3",      required_argument, NULL,  FLAG_IM_3},
         {0, 0, 0, 0}
     };
 
@@ -114,16 +101,10 @@ int main (int argc, char** argv)
     // ===============================================  
     int i;
     int option_index = 0;
-    while ( ( i = getopt_long(argc, argv, "o:t:usah", long_options, &option_index) ) != EOF) {
+    while ( ( i = getopt_long(argc, argv, "i:o:t:h", long_options, &option_index) ) != EOF) {
         switch (i) {
-            case FLAG_IM_1:
-                inputFileName1.assign( optarg );
-                break;
-            case FLAG_IM_2:
-                inputFileName2.assign( optarg );
-                break;
-            case FLAG_IM_3:
-                inputFileName3.assign( optarg );
+            case 'i':
+                inputFileName.assign( optarg );
                 break;
             case 'o':
                 outputFileName.assign(optarg);
@@ -145,20 +126,15 @@ int main (int argc, char** argv)
 
 
     if (
-        argc != 0 ||  inputFileName1.length() == 0
-            || inputFileName2.length() == 0
-            || inputFileName3.length() == 0
+        argc != 0 ||  inputFileName.length() == 0
             || outputFileName.length() == 0
-            || ( dataTypeOut != svkImageWriterFactory::DICOM_MRI && dataTypeOut != svkImageWriterFactory::IDF )
+            || ( dataTypeOut != svkImageWriterFactory::DICOM_MRS && dataTypeOut != svkImageWriterFactory::DDF )
     ) {
             cout << usemsg << endl;
             exit(1); 
     }
 
-
-    cout << inputFileName1 << endl;
-    cout << inputFileName2 << endl;
-    cout << inputFileName3 << endl;
+    cout << inputFileName << endl;
     cout << outputFileName << endl;
 
 
@@ -167,34 +143,28 @@ int main (int argc, char** argv)
     //  of the correct type for the input file format.
     // ===============================================
     svkImageReaderFactory* readerFactory = svkImageReaderFactory::New();
-    svkImageReader2* reader1 = readerFactory->CreateImageReader2(inputFileName1.c_str());
-    svkImageReader2* reader2 = readerFactory->CreateImageReader2(inputFileName2.c_str());
-    svkImageReader2* reader3 = readerFactory->CreateImageReader2(inputFileName3.c_str());
+    svkImageReader2* reader = readerFactory->CreateImageReader2(inputFileName.c_str());
     readerFactory->Delete();
 
-    if (reader1 == NULL || reader2 == NULL || reader3 == NULL) {
-        cerr << "Can not determine appropriate reader for: " << inputFileName1 << ", " 
-             << inputFileName2 << " or " << inputFileName3 <<  endl;
+    if (reader == NULL ) {
+        cerr << "Can not determine appropriate reader for: " << inputFileName << endl; 
         exit(1);
     }
 
     //  Read the data to initialize an svkImageData object
-    reader1->SetFileName( inputFileName1.c_str() );
-    reader1->Update();
-    reader2->SetFileName( inputFileName2.c_str() );
-    reader2->Update();
-    reader3->SetFileName( inputFileName3.c_str() );
-    reader3->Update();
-
+    reader->SetFileName( inputFileName.c_str() );
+    reader->Update();
+    
+    //reader->GetOutput()->GetDcmHeader()->PrintDcmHeader(); 
 
     // ===============================================  
     //  Pass data through your algorithm:
     // ===============================================  
-    svkDynamicMRIAlgoTemplate* dynamics = svkDynamicMRIAlgoTemplate::New();
-    dynamics->SetInputConnection( 0, reader1->GetOutputPort() ); 
-    dynamics->SetInputConnection( 1, reader2->GetOutputPort() ); 
-    dynamics->SetInputConnection( 2, reader3->GetOutputPort() ); 
-    dynamics->Update();
+    svkMRSAutoPhase* phaser = svkMRSAutoPhase::New();
+    phaser->SetInputConnection(0, reader->GetOutputPort(0) ); 
+    phaser->SetPhasingModel(svkMRSAutoPhase::MIN_AREA_MAX_POSITIVE_0); 
+    //phaser->SetPhasingModel(svkMRSAutoPhase::MIN_MAGNITUDE_DIFF_0); 
+    phaser->Update();
 
 
     // ===============================================  
@@ -202,11 +172,11 @@ int main (int argc, char** argv)
     //  output file format. 
     // ===============================================  
     svkImageWriterFactory* writerFactory = svkImageWriterFactory::New();
-    svkImageWriter* writer = static_cast<svkImageWriter*>( writerFactory->CreateImageWriter( svkImageWriterFactory::DICOM_MRI ) ); 
+    svkImageWriter* writer = static_cast<svkImageWriter*>( writerFactory->CreateImageWriter( dataTypeOut ) ); 
     writerFactory->Delete();
     
     if ( writer == NULL ) {
-        cerr << "Can not create writer of type: " << svkImageWriterFactory::DICOM_MRS << endl;
+        cerr << "Can not create writer " << endl;
         exit(1);
     }
    
@@ -215,16 +185,15 @@ int main (int argc, char** argv)
     // ===============================================  
     //  Write output of algorithm to file:    
     // ===============================================  
-    writer->SetInput( dynamics->GetOutput() );
+    //writer->SetInput( phaser->GetOutput()); 
+    writer->SetInput( phaser->GetImageDataInput(0) );
     writer->Write();
 
 
     //  clean up:
-    dynamics->Delete(); 
+    phaser->Delete(); 
     writer->Delete();
-    reader1->Delete();
-    reader2->Delete();
-    reader3->Delete();
+    reader->Delete();
 
     return 0; 
 }
