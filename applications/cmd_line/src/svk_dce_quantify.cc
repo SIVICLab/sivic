c97d39d2bf07f55fd4fd6a53b9f6c6a03c70d63a
James Hawkins <james.hawkins@ucsf.edu>
2015-12-17 15:03:50 -0800

    washoutPosWriter fix

--- applications/cmd_line/src/svk_dce_quantify.cc
+++ applications/cmd_line/src/svk_dce_quantify.cc
@@ -305,6 +305,7 @@ int main (int argc, char** argv)
     peakTimeWriter->Write();
     maxSlopeWriter->Write();
     washoutWriter->Write();
+    washoutPosWriter->Write();
 
     // ===============================================  
 
