45aa37424bbfb4c7cdfcea70eccad92b29481680
Crane, Jason <jason.crane@ucsf.edu>
2019-11-15 18:34:05 -0800

    created stubs for auto freq correct functionality

---
+++ applications/cmd_line/src/svk_auto_freq_correct.cc
@@ -0,0 +1,268 @@
+/*
+ *  Copyright © 2009-2017 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name
+ *      "The Regents of the University of California," or the names of any of its
+ *      contributors may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ *  OF SUCH DAMAGE. 
+ */     
+
+
+/*
+ *  $URL$
+ *  $Rev$
+ *  $Author$
+ *  $Date$
+ *
+ *  Authors:
+ *   
+ *  
+ *
+ *  Utility application for converting between supported file formats. 
+ *
+ */
+
+
+#include <vtkSmartPointer.h>
+
+#include <svkImageReaderFactory.h>
+#include <svkImageReader2.h>
+#include <svkImageWriterFactory.h>
+#include <svkImageWriter.h>
+#include <svkMRSAutoFreqCorrect.h>
+#include <svkMRSMatchPeakFreq.h>
+#include <svkMRSZeroOrderPhase.h>
+
+#ifdef WIN32
+extern "C" {
+#include <getopt.h>
+}
+#else
+#include <getopt.h>
+#endif
+
+
+using namespace svk;
+
+
+int main (int argc, char** argv)
+{
+
+    string usemsg("\n") ;
+    usemsg += "Version " + string(SVK_RELEASE_VERSION) +                                           "\n";
+    usemsg += "svk_auto_freq_correct -i input_filename -o output_filename -a type                   \n"; 
+    usemsg += "                 --peak_loc freq  --win freq                                         \n";
+    usemsg += "               [ -t output_data_type ]                                               \n";
+    usemsg += "               [ --single ] [ -h ]                                                   \n";
+    usemsg += "                                                                                     \n";
+    usemsg += "   -i            name   Name of input MRS file.                                      \n";
+    usemsg += "   -o            root   Root of output file.                                         \n";
+    usemsg += "   -t            type   Target data type:                                            \n";
+    usemsg += "                                 2 = UCSF DDF and IDF freq shift map                 \n";
+    usemsg += "                                 4 = DICOM_MRS and DCM freq shift map (default)      \n";
+    usemsg += "   -a            type   method for frequency correction                              \n";
+    usemsg += "                                 1 = match peak location (default)                   \n";
+    usemsg += "   --peak_loc    freq   Correct frequency location for peak in window (--win)        \n";  
+    usemsg += "   --win         freq   Frequency window within which the ref peak should be found.  \n";  
+    usemsg += "   --single             Only correct specified file if multiple in series            \n";
+    usemsg += "   -u            units  Units for --peak_loc and --win flags                         \n";
+    usemsg += "                         1 = PPM                                                     \n";
+    usemsg += "                         2 = HZ                                                      \n";
+    usemsg += "                         3 = PTS (default)                                           \n";
+    usemsg += "   -h                   Print this help mesage.                                      \n";
+    usemsg += "                                                                                     \n";
+    usemsg += "Auto detect freq shift for spectra.                                                  \n";
+    usemsg += "     1:  match peak locaion in specified frequency window                            \n";
+    usemsg += "                                                                                     \n";
+
+
+    string inputFileName;
+    string outputFileName;
+    svkImageWriterFactory::WriterType mrsDataTypeOut = svkImageWriterFactory::DICOM_MRS;
+    svkImageWriterFactory::WriterType mriDataTypeOut = svkImageWriterFactory::DICOM_ENHANCED_MRI;
+    svkMRSAutoFreqCorrect::PhasingModel phasingType = svkMRSAutoFreqCorrect::UNDEFINED_PHASE_MODEL; 
+
+    bool onlyPhaseSingle = false;
+
+    string cmdLine = svkProvenance::GetCommandLineString( argc, argv );
+
+    enum FLAG_NAME {
+        FLAG_SINGLE = 0
+    };
+
+
+    static struct option long_options[] =
+    {
+        {"single",    no_argument,       NULL,  FLAG_SINGLE},
+        {0, 0, 0, 0}
+    };
+
+
+    // ===============================================  
+    //  Process flags and arguments
+    // ===============================================  
+    int i;
+    int option_index = 0;
+    while ( ( i = getopt_long(argc, argv, "i:o:t:a:h", long_options, &option_index) ) != EOF) {
+        switch (i) {
+            case 'i':
+                inputFileName.assign( optarg );
+                break;
+            case 'o':
+                outputFileName.assign(optarg);
+                //  Make sure the file name doesn't contain an extension: 
+                outputFileName = svkImageReader2::GetFileRoot(outputFileName.c_str()); 
+                break;
+            case 't':
+                mrsDataTypeOut = static_cast<svkImageWriterFactory::WriterType>( atoi(optarg) );
+                break;
+            case 'a':
+                phasingType = static_cast<svkMRSAutoFreqCorrect::PhasingModel>( atoi(optarg) );
+                break;
+            case FLAG_SINGLE:
+                onlyPhaseSingle = true;
+                break;
+            case 'h':
+                cout << usemsg << endl;
+                exit(1);
+                break;
+            default:
+                ;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+
+    if (
+        argc != 0 ||  inputFileName.length() == 0
+            || outputFileName.length() == 0
+            || ( mrsDataTypeOut != svkImageWriterFactory::DICOM_MRS && mrsDataTypeOut != svkImageWriterFactory::DDF )
+            || outputFileName.length() == 0
+            || phasingType == svkMRSAutoFreqCorrect::UNDEFINED_PHASE_MODEL 
+    ) {
+            cout << usemsg << endl;
+            exit(1); 
+    }
+
+    if ( phasingType <= svkMRSAutoFreqCorrect::UNDEFINED_PHASE_MODEL || phasingType >= svkMRSAutoFreqCorrect::LAST_MODEL ) {
+        cout << "Specified phasing type is not recognized." << endl;
+        cout << usemsg << endl;
+        exit(1); 
+    }
+
+    // ===============================================
+    //  Output phase image type is implied by output MRS data type:  
+    // ===============================================
+    if ( mrsDataTypeOut == svkImageWriterFactory::DDF ) {
+        mriDataTypeOut = svkImageWriterFactory::IDF;
+    }
+
+    if( ! svkUtils::FilePathExists( inputFileName.c_str() ) ) {
+        cerr << "Input file can not be loaded (may not exist) " << inputFileName << endl;
+        exit(1);
+    }
+
+    cout << "Input File:  " << inputFileName << endl;
+    cout << "Output File: " << outputFileName << endl;
+    cout << "Phase Type:  " << phasingType << endl;
+
+
+    // ===============================================
+    //  Use a reader factory to create a data reader 
+    //  of the correct type for the input file format.
+    // ===============================================
+    svkImageReaderFactory* readerFactory = svkImageReaderFactory::New();
+    svkImageReader2* reader = readerFactory->CreateImageReader2(inputFileName.c_str());
+    readerFactory->Delete();
+
+    if (reader == NULL ) {
+        cerr << "Can not determine appropriate reader for: " << inputFileName << endl; 
+        exit(1);
+    }
+
+    //  Read the data to initialize an svkImageData object
+    reader->SetFileName( inputFileName.c_str() );
+    if ( onlyPhaseSingle == true ) {
+        reader->OnlyReadOneInputFile();
+    }
+    //reader->Update();
+    reader->GetOutput()->GetProvenance()->SetApplicationCommand( cmdLine );
+    
+
+    // ===============================================  
+    //  Pass data through phasing algo.  Should use a factory to get
+    //  correcty phasing algo:
+    // ===============================================  
+    svkMRSAutoFreqCorrect* phaser;
+    if ( phasingType == svkMRSAutoFreqCorrect::PEAK_LOC) {  
+        phaser = svkMRSMatchPeakFreq::New();
+    } else {
+        cout << "Specified phasing type is not recognized." << endl;
+        exit(1); 
+    }
+    phaser->SetInputConnection(0, reader->GetOutputPort(0) ); 
+    phaser->OnlyUseSelectionBox();
+    phaser->Update();
+    phaser->GetOutput(0)->GetProvenance()->SetApplicationCommand( cmdLine );
+
+
+    // ===============================================  
+    //  Use writer factory to create writer for specified
+    //  output file format. 
+    // ===============================================  
+    svkImageWriterFactory* writerFactory = svkImageWriterFactory::New();
+    svkImageWriter* mrsWriter = static_cast<svkImageWriter*>( writerFactory->CreateImageWriter( mrsDataTypeOut ) ); 
+    svkImageWriter* mriWriter = static_cast<svkImageWriter*>( writerFactory->CreateImageWriter( mriDataTypeOut ) ); 
+    writerFactory->Delete();
+    
+    if ( mrsWriter == NULL || mriWriter == NULL ) {
+        cerr << "Can not create writer " << endl;
+        exit(1);
+    }
+
+    mrsWriter->SetFileName( outputFileName.c_str() );
+    string phaseMapName = outputFileName;
+    if ( phasingType == svkMRSAutoFreqCorrect::PEAK_LOC) {  
+        phaseMapName.append("_PeakLocMap"); 
+    } 
+    mriWriter->SetFileName( phaseMapName.c_str() );
+
+    // ===============================================  
+    //  Write output of algorithm to file:    
+    // ===============================================  
+    mrsWriter->SetInputData( phaser->GetImageDataInput(0) );
+    mrsWriter->Write();
+    mriWriter->SetInputData( phaser->GetOutput(0) );
+    mriWriter->Write();
+
+    //  clean up:
+    phaser->Delete(); 
+    mrsWriter->Delete();
+    mriWriter->Delete();
+    reader->Delete();
+
+    return 0; 
+}
+
+
---
+++ libs/src/svkMRSMatchPeakFreq.cc
@@ -0,0 +1,349 @@
+/*
+ *  Copyright © 2009-2017 The Regents of the University of California.
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met:
+ *  •   Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *  •   Redistributions in binary form must reproduce the above copyright notice, 
+ *      this list of conditions and the following disclaimer in the documentation 
+ *      and/or other materials provided with the distribution.
+ *  •   None of the names of any campus of the University of California, the name 
+ *      "The Regents of the University of California," or the names of any of its 
+ *      contributors may be used to endorse or promote products derived from this 
+ *      software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ *  OF SUCH DAMAGE.
+ */
+
+
+
+/*
+ *  $URL$
+ *  $Rev$
+ *  $Author$
+ *  $Date$
+ *
+ *  Authors:
+ *      Jason C. Crane, Ph.D.
+ *      Beck Olson,
+ */
+
+
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
+#include <vtkImageFourierFilter.h> // for vtkImageComplex struct
+#include <vtkMath.h>
+
+#include <svkMRSZeroOrderPhaseCostFunction.h>
+#include <svkMRSMatchPeakFreq.h>
+#include <svkPhaseSpec.h>
+#include <svkMrsImageFFT.h>
+
+
+using namespace svk;
+
+
+//vtkCxxRevisionMacro(svkMRSMatchPeakFreq, "$Rev$");
+vtkStandardNewMacro(svkMRSMatchPeakFreq);
+
+
+/*!
+ *
+ */
+svkMRSMatchPeakFreq::svkMRSMatchPeakFreq()
+{
+#if VTK_DEBUG_ON
+    this->DebugOn();
+#endif
+
+    vtkDebugMacro(<< this->GetClassName() << "::" << this->GetClassName() << "()");
+    this->seriesDescription = "MatchPeakFreqMap"; 
+}
+
+
+/*!
+ *
+ */
+svkMRSMatchPeakFreq::~svkMRSMatchPeakFreq()
+{
+}
+
+/*!
+ *  Copy the Dcm Header and Provenance from the input to the output. 
+ */
+int svkMRSMatchPeakFreq::RequestData( vtkInformation* request, vtkInformationVector** inputVector, vtkInformationVector* outputVector )
+{
+    this->InitPeakPicker ( ); 
+    return this->Superclass::RequestData(request, inputVector, outputVector); 
+    
+}
+
+/*!
+ *  Make sure data is in frequency domain for zero order phasing
+ */
+void svkMRSMatchPeakFreq::PrePhaseSetup()
+{
+
+    svkMrsImageData* data = svkMrsImageData::SafeDownCast(this->GetImageDataInput(0));
+
+    //  Get the domain of the input spectra:  time/frequency
+    string spectralDomain = data->GetDcmHeader()->GetStringValue( "SignalDomainColumns");
+    if ( spectralDomain.compare("TIME") == 0 ) {
+        this->isSpectralFFTRequired = true;
+        cout << "TIME DOMAIN INPUT" << endl;
+    } else {
+        this->isSpectralFFTRequired = false;
+        cout << "FREQ DOMAIN INPUT" << endl;
+    }
+
+    //  if necessary, transform data to time domain: 
+    if ( this->isSpectralFFTRequired == true ) {
+
+        svkMrsImageFFT* fft = svkMrsImageFFT::New();
+        fft->SetInputData( data );
+
+        fft->SetFFTDomain( svkMrsImageFFT::SPECTRAL );
+
+        //  frequency to time: 
+        fft->SetFFTMode( svkMrsImageFFT::REVERSE );
+
+        fft->Update();
+        fft->Delete();
+    
+    }
+
+}
+
+
+/*!
+ *  If a pre FFT was performed to get data in frequency domain, undo that here: 
+ */
+void svkMRSMatchPeakFreq::PostPhaseCleanup()
+{
+
+    //  if necessary, transform data to time domain: 
+    if ( this->isSpectralFFTRequired == true) {
+
+        svkMrsImageData* data = svkMrsImageData::SafeDownCast(this->GetImageDataInput(0));
+
+        svkMrsImageFFT* fft = svkMrsImageFFT::New();
+        fft->SetInputData( data );
+
+        fft->SetFFTDomain( svkMrsImageFFT::SPECTRAL );
+
+        //  frequency to time: 
+        fft->SetFFTMode( svkMrsImageFFT::FORWARD );
+
+        fft->Update();
+        fft->Delete();
+
+    }
+
+}
+
+
+/*!
+ * 
+ */
+#ifdef SWARM
+void svkMRSMatchPeakFreq::InitOptimizer( int cellID, itk::ParticleSwarmOptimizer::Pointer itkOptimizer )
+#else
+void svkMRSMatchPeakFreq::InitOptimizer( int cellID, itk::PowellOptimizer::Pointer itkOptimizer )
+#endif
+
+{
+
+    svkMrsImageData* data = svkMrsImageData::SafeDownCast(this->GetImageDataInput(0));
+    vtkFloatArray* spectrum = static_cast<vtkFloatArray*>( data->GetSpectrum( cellID ) );
+    float cmplxPt[2];
+    spectrum->GetTupleValue(0, cmplxPt);
+    //cout << " cell: " << cellID << " -> " << cmplxPt[0] << endl;
+
+    //========================================================
+    //  ITK Optimization 
+    //========================================================
+    svkMRSZeroOrderPhaseCostFunction::Pointer costFunction = svkMRSZeroOrderPhaseCostFunction::New();
+    itkOptimizer->SetCostFunction( costFunction.GetPointer() );
+
+    costFunction->SetSpectrum( spectrum ); 
+    costFunction->SetNumFreqPoints(this->numTimePoints ); 
+    costFunction->SetPeakPicker( this->peakPicker ); 
+
+    //  set dimensionality of parameter space: 
+    const unsigned int spaceDimension = costFunction->GetNumberOfParameters();
+    typedef svkPhaseCostFunction::ParametersType ParametersType;
+    ParametersType  initialPosition( spaceDimension );
+    initialPosition[0] =  0.;
+
+#ifdef SWARM 
+
+        //  Set bounds
+        itk::ParticleSwarmOptimizer::ParameterBoundsType bounds;
+        // Zero order phase range radians
+        bounds.push_back( std::make_pair( -1 * vtkMath::Pi(), vtkMath::Pi()) ); 
+        itkOptimizer->SetParameterBounds( bounds );
+
+        unsigned int numberOfParticles = 10;
+        unsigned int maxIterations = 500;
+        double xTolerance = 0.01;
+        double fTolerance = 0.01;
+        itk::ParticleSwarmOptimizer::ParametersType 
+            initialParameters( spaceDimension ), finalParameters;
+        itkOptimizer->SetNumberOfParticles( numberOfParticles );
+        itkOptimizer->SetMaximalNumberOfIterations( maxIterations );
+        itkOptimizer->SetParametersConvergenceTolerance( xTolerance,
+                                                   costFunction->GetNumberOfParameters() );
+        itkOptimizer->SetFunctionConvergenceTolerance( fTolerance );
+        itkOptimizer->SetInitializeNormalDistribution( false ); // use uniform distribution
+
+        //  required to get reproducible results, otherwise uses random seed
+        itkOptimizer->SetUseSeed( true ); // use uniform distribution
+        itkOptimizer->SetSeed( 7 ); // use uniform distribution
+
+
+#else
+        itkOptimizer->SetStepLength( 1 );
+        itkOptimizer->SetStepTolerance( 1);
+        itkOptimizer->SetValueTolerance( 1 );
+        itkOptimizer->SetMaximumIteration( 10000 );
+#endif 
+
+    itkOptimizer->SetInitialPosition( initialPosition );
+
+}
+
+
+/*!
+ * 
+ */
+void svkMRSMatchPeakFreq::FitPhase( int cellID )  
+{
+
+#ifdef SWARM 
+        typedef itk::ParticleSwarmOptimizer OptimizerType;
+#else
+        typedef itk::PowellOptimizer        OptimizerType;
+#endif
+
+    OptimizerType::Pointer itkOptimizer = OptimizerType::New();
+    this->InitOptimizer( cellID, itkOptimizer ); 
+
+#ifndef SWARM 
+    //  maximize total peak height
+    itkOptimizer->SetMaximize( true );
+#endif
+
+    try { 
+        cout << "CELL ID Optimize: " << cellID << endl;
+        itkOptimizer->StartOptimization();
+    } catch( itk::ExceptionObject & e ) {
+        cout << "Exception thrown ! " << endl;
+        cout << "An error ocurred during Optimization" << endl;
+        cout << "Location    = " << e.GetLocation()    << endl;
+        cout << "Description = " << e.GetDescription() << endl;
+        //return EXIT_FAILURE;
+        return; 
+    }
+
+    typedef svkPhaseCostFunction::ParametersType ParametersType;
+    ParametersType finalPosition = itkOptimizer->GetCurrentPosition();
+
+    //
+    // check results to see if it is within range
+    //
+    bool pass = true;
+
+    // Exercise various member functions.
+    cout << endl;
+
+    //itkOptimizer->Print( cout );
+
+    if( !pass ) {
+        cout << "Test failed." << endl;
+        return;
+    } 
+    //cout << "Test passed." << endl;
+
+    //  ===================================================
+    //  Apply fitted phase values to spectrum: 
+    //  ===================================================
+    svkMrsImageData* data = svkMrsImageData::SafeDownCast(this->GetImageDataInput(0));
+    vtkFloatArray* spectrum = static_cast<vtkFloatArray*>( data->GetSpectrum( cellID ) );
+
+    float phi0Final = finalPosition[0]; 
+    cout << "           PHI0 FINAL("<<cellID <<"): " << phi0Final * 180. / vtkMath::Pi() << endl;
+    svkPhaseSpec::ZeroOrderPhase( phi0Final, spectrum); 
+
+
+    //  ===================================================
+    //  Write the fitted phase values to a parameter map: 
+    //  ===================================================
+    svkMriImageData* mriData = svkMriImageData::SafeDownCast(this->GetOutput(0));
+
+    //  map the mrsCellID to the correct output map volume:     
+    svkDcmHeader::DimensionVector mriDimensionVector = mriData->GetDcmHeader()->GetDimensionIndexVector();
+    int numSpatialVoxels = svkDcmHeader::GetNumSpatialVoxels(&mriDimensionVector);
+    int volumeNumber = static_cast<int>(cellID/numSpatialVoxels); 
+    this->mapArrayZeroOrderPhase= mriData->GetPointData()->GetArray(volumeNumber);
+    //this->mapArrayMatchPeakFreq->SetName( "MatchPeakFreq" ); 
+    float phi0FinalDegrees = phi0Final * 180. / vtkMath::Pi(); 
+
+    //  put this in the correct spatial location: 
+    svkDcmHeader::DimensionVector loopVector = mriDimensionVector; 
+    svkDcmHeader::GetDimensionVectorIndexFromCellID( &mriDimensionVector, &loopVector, cellID); 
+    int spatialCellID = mriData->GetDcmHeader()->GetSpatialCellIDFromDimensionVectorIndex( &mriDimensionVector, &loopVector); 
+    this->mapArrayZeroOrderPhase->SetTuple1(spatialCellID, phi0FinalDegrees);
+
+    return;
+}
+
+
+void svkMRSMatchPeakFreq::ValidateInput()
+{
+}
+
+
+/*!
+ *  Return the peak with the largest amplitude
+ *  Used if only phasing on 1 peak
+ */
+int svkMRSMatchPeakFreq::GetLargestMatchPeakFreqPeak( )
+{
+    int peakNum; 
+    // Pick the peak for zero order phasing: 
+    float height = 0; 
+    for ( int i = 1; i < this->peakPicker->GetNumPeaks() - 1 ; i++ ) {
+        float tmpHt = this->peakPicker->GetAvRMSPeakHeight(i); 
+        if ( tmpHt > height ) { 
+            peakNum = i; 
+        }
+    }
+    return peakNum; 
+}
+
+/*!
+ * 
+ */
+void svkMRSMatchPeakFreq::InitPeakPicker ( ) 
+{
+    svkMrsImageData* data = svkMrsImageData::SafeDownCast(this->GetImageDataInput(0));
+    this->peakPicker = svkMRSPeakPick::New();
+    this->peakPicker->SetInputData( data );
+    if ( this->onlyUseSelectionBox == true ) {
+        this->peakPicker->OnlyUseSelectionBox();
+    }
+    this->peakPicker->Update();  
+
+}
